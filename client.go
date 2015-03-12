6702d378972258a50ed6632ec02d0480bf84aaec
Trifonov Grigory <trifonov.grigory@gmail.com>
2015-03-12 14:53:00 +0300

    Working prototype.

--- client.go
+++ client.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"./shared"
 	"errors"
 	"fmt"
 	"github.com/gopherjs/websocket"
 	"github.com/tgregory/nats"
+	"github.com/tgregory/nats-websocket/shared"
 	"honnef.co/go/js/dom"
+	//"io"
 	"math/rand"
 	"net"
 	"time"
@@ -32,67 +33,92 @@ var (
 	TimedOut = errors.New("Dial timed out.")
 )
 
+type ConnMonitor struct {
+	net.Conn
+}
+
 type WebsocketTimeoutDialer struct {
 }
 
 func (wtd WebsocketTimeoutDialer) DialTimeout(host string, timeout time.Duration) (net.Conn, error) {
+	host = "ws://localhost:8888/nats"
+	println("Preparing to dial: ", host)
 	result := make(chan net.Conn, 1)
 	err := make(chan error, 1)
 	tout := time.After(timeout)
 	go func() {
+		println("Dialing.")
 		conn, e := websocket.Dial(host)
 		if nil != e {
 			err <- e
 		} else {
-			result <- conn
+			result <- &shared.MonitoredConn{conn}
 		}
 	}()
 	select {
 	case res := <-result:
+		println("Dial success.")
 		return res, nil
 	case e := <-err:
+		println("Dial error.")
+		println(e.Error())
 		return nil, e
 	case <-tout:
+		println("Dial timeout.")
 		return nil, TimedOut
 	}
 }
 
 func main() {
-	conn, err := nats.ConnectDialer("ws://localhost/nats", WebsocketTimeoutDialer{})
-	if nil != err {
-		panic(err)
-	}
-	ec, err := nats.NewEncodedConn(conn, "json")
-	if nil != err {
-		panic(err)
-	}
-	defer ec.Close()
-	recvCh := make(chan *shared.Message)
-	ec.BindRecvChan("messages", recvCh)
-
-	sendCh := make(chan *shared.Message, 10)
-	ec.BindSendChan("messages", sendCh)
+	rand.Seed(time.Now().UnixNano())
+	sendCh := make(chan *shared.Message, 1)
+	recvCh := make(chan *shared.Message, 1)
+	author := fmt.Sprintf("%v %v", names[rand.Intn(5)], lastNames[rand.Intn(5)])
+	go func() {
+		conn, err := nats.ConnectDialer("ws://localhost:8888", WebsocketTimeoutDialer{})
+		if nil != err {
+			fmt.Printf("Nats connect failed: %v\n", err)
+			return
+		}
+		//defer conn.Close()
+		ec, err := nats.NewEncodedConn(conn, "json")
+		if nil != err {
+			fmt.Println(err.Error())
+			return
+		}
+		ec.Flush()
+		ec.BindRecvChan("messages", recvCh)
+		ec.BindSendChan("messages", sendCh)
+		ec.Subscribe("join", func(author string) {
+			fmt.Println(author + " joined")
+		})
+		ec.Publish("join", author)
+		ec.Flush()
+	}()
 	d := dom.GetWindow().Document()
-	author := fmt.Sprintf("%v %v", names[rand.Int31n(5)], lastNames[rand.Int31n(5)])
-	d.GetElementByID("#user").(*dom.BasicElement).SetInnerHTML(author)
-	messageInput := d.GetElementByID("#message").(*dom.BasicElement)
-	d.GetElementByID("#send").(*dom.BasicElement).AddEventListener("click", false, func(evt dom.Event) {
-		data := messageInput.TextContent()
+	d.GetElementByID("user").(*dom.HTMLDivElement).SetInnerHTML(author)
+	messageInput := d.GetElementByID("message").(*dom.HTMLInputElement)
+	d.GetElementByID("send").(*dom.HTMLButtonElement).AddEventListener("click", false, func(evt dom.Event) {
+		fmt.Println("Clicked.")
+		data := messageInput.Value
 		timestamp := time.Now()
-		messageInput.SetTextContent("")
+		messageInput.Value = ""
 		go func() {
-			sendCh <- &shared.Message{
+			msg := &shared.Message{
 				Timestamp: timestamp,
 				Author:    author,
 				Data:      data,
 			}
+			sendCh <- msg
 		}()
 	})
-	messageBoard := d.GetElementByID("#message-board").(*dom.BasicElement)
+	messageBoard := d.GetElementByID("message-board").(*dom.HTMLTableElement)
+	//go func() {
 	for {
 		mes := <-recvCh
 		mesEl := d.CreateElement("tr")
-		mesEl.SetInnerHTML(fmt.Sprintf("<td>%v</td><td>%v</td><td>%v</td>", mes.Timestamp, mes.Author, mes.Data))
+		mesEl.SetInnerHTML(fmt.Sprintf("<td style='width: 4em;'>%v</td><td style='width: 10em;'>%v</td><td>%v</td>", mes.Timestamp.Format(time.Kitchen), mes.Author, mes.Data))
 		messageBoard.AppendChild(mesEl)
 	}
+	//}()
 }
--- server.go
+++ server.go
@@ -1,15 +1,94 @@
 package main
 
 import (
+	"compress/gzip"
 	"errors"
 	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/tgregory/gnatsd/logger"
 	"github.com/tgregory/gnatsd/server"
-	"golang.org/x/net/websocket"
+	//"github.com/tgregory/nats-websocket/shared"
+	//"golang.org/x/net/websocket"
+	"io"
 	"net"
 	"net/http"
 	"runtime"
+	"strings"
 	"sync"
+	"time"
 )
+
+var (
+	Closed = errors.New("Socket closed.")
+)
+
+type WebsocketConn struct {
+	*websocket.Conn
+	r io.Reader
+}
+
+func (wc *WebsocketConn) Read(b []byte) (n int, err error) {
+	var tpe int
+	var r io.Reader
+	for nil == wc.r && nil == err {
+		tpe, r, err = wc.NextReader()
+		if nil != err {
+			break
+		}
+		if tpe == websocket.BinaryMessage {
+			wc.r = r
+		}
+	}
+	if nil != err {
+		fmt.Println(err)
+		return 0, err
+	}
+	n, err = wc.r.Read(b)
+	if io.EOF == err {
+		err = nil
+		wc.r = nil
+	}
+	if n != 0 {
+		fmt.Println("Read: ", string(b[:n]))
+	}
+	return n, err
+}
+
+func (wc *WebsocketConn) SetDeadline(t time.Time) error {
+	if err := wc.SetReadDeadline(t); nil != err {
+		return err
+	}
+	return wc.SetWriteDeadline(t)
+}
+
+func (wc *WebsocketConn) Write(b []byte) (n int, err error) {
+	n, err = len(b), wc.WriteMessage(websocket.BinaryMessage, b)
+	fmt.Println("Written: ", string(b[:n]))
+	return n, err
+}
+
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func makeGzipHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			fn(w, r)
+			return
+		}
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
+		gzr := gzipResponseWriter{Writer: gz, ResponseWriter: w}
+		fn(gzr, r)
+	}
+}
 
 var (
 	ListenerClosed = errors.New("Listener closed.")
@@ -37,23 +116,38 @@ type websocketListener struct {
 func Listen(laddr string) (*websocketListener, error) {
 	fmt.Println("Creating websocket listener.")
 	lst := &websocketListener{
-		addr:  WebsocketAddr(fmt.Sprintf("%v%v", laddr, "/nats")),
+		addr:  WebsocketAddr(laddr), //WebsocketAddr(fmt.Sprintf("%v%v", laddr, "/nats")),
 		snc:   &sync.RWMutex{},
-		queue: make(chan net.Conn, 32),
+		queue: make(chan net.Conn, 1),
 		err:   make(chan error),
 	}
 	fmt.Println(lst.Addr())
 	mux := http.NewServeMux()
-	mux.Handle("/nats", websocket.Handler(func(ws *websocket.Conn) {
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  10,
+		WriteBufferSize: 10,
+	}
+	/*mux.Handle("/nats", websocket.Handler(func(ws *websocket.Conn) {
 		if nil != ws {
 			lst.snc.RLock()
-			defer lst.snc.Unlock()
+			defer lst.snc.RUnlock()
 			if !lst.closed {
-				lst.queue <- ws
+				lst.queue <- &shared.MonitoredConn{ws}
 			}
 		}
-	}))
-	mux.Handle("/", http.FileServer(http.Dir("./")))
+	}))*/
+	mux.HandleFunc("/nats", func(w http.ResponseWriter, r *http.Request) {
+		ws, err := upgrader.Upgrade(w, r, nil)
+		if nil == err {
+			lst.snc.RLock()
+			defer lst.snc.RUnlock()
+			if !lst.closed {
+				lst.queue <- &WebsocketConn{ws, nil}
+				//lst.queue <- &shared.MonitoredConn{ws.UnderlyingConn()}
+			}
+		}
+	})
+	mux.HandleFunc("/", makeGzipHandler(http.FileServer(http.Dir("./")).ServeHTTP))
 	lst.srv = &http.Server{
 		Handler: mux,
 		Addr:    laddr,
@@ -61,6 +155,7 @@ func Listen(laddr string) (*websocketListener, error) {
 	go func() {
 		fmt.Println("Starting connections accept.")
 		e := lst.srv.ListenAndServe()
+		//e := http.ListenAndServe(laddr, mux)
 		fmt.Println(e)
 		lst.err <- e
 	}()
@@ -72,11 +167,13 @@ func (wl *websocketListener) Accept() (net.Conn, error) {
 	fmt.Println("Accepting.")
 	select {
 	case con := <-wl.queue:
+		fmt.Printf("%+v\n", con)
 		if nil == con {
 			return nil, ListenerClosed
 		}
 		return con, nil
 	case err := <-wl.err:
+		fmt.Println(err)
 		wl.Close()
 		return nil, err
 	}
@@ -101,12 +198,18 @@ func (wlc WebsocketListenerCreator) CreateListener(laddr string) (net.Listener, 
 
 func main() {
 	srv := server.NewCustom(&server.Options{
-		Host:     "localhost",
+		Host:     "",
 		Port:     8888,
-		HTTPPort: 20000,
+		HTTPPort: 8887,
 	}, WebsocketListenerCreator{})
+	/*srv := server.NewCustom(&server.Options{
+		Host: "",
+		Port: 8888,
+	}, nil)*/
+	srv.SetLogger(logger.NewFileLogger("server.log", true, true, true, true), true, true)
 	srv.Start()
 	for {
+		time.Sleep(1 * time.Minute)
 		runtime.Gosched()
 	}
 }
