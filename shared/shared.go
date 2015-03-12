b8995935a73086bd4f9be12f64caa6551f29fef3
Trifonov Grigory <trifonov.grigory@gmail.com>
2015-03-12 14:18:42 +0300

    Monitored connection for easies debug.

--- shared/shared.go
+++ shared/shared.go
@@ -1,6 +1,8 @@
 package shared
 
 import (
+	"fmt"
+	"net"
 	"time"
 )
 
@@ -8,4 +10,32 @@ type Message struct {
 	Author    string
 	Timestamp time.Time
 	Data      string
+}
+
+type MonitoredConn struct {
+	net.Conn
+}
+
+func (mc *MonitoredConn) Read(b []byte) (n int, err error) {
+	n, err = mc.Conn.Read(b)
+	if nil != err {
+		fmt.Println(err)
+	} else {
+		if 0 != n {
+			fmt.Println("Read: ", string(b[:n]))
+		}
+	}
+	return n, err
+}
+
+func (mc *MonitoredConn) Write(b []byte) (n int, err error) {
+	n, err = mc.Conn.Write(b)
+	if nil != err {
+		fmt.Println(err)
+	} else {
+		if 0 != n {
+			fmt.Println("Written: ", string(b[:n]))
+		}
+	}
+	return n, err
 }
